f9d87062e06b424114acb7a2b4c6707eff2102dc
kakakepi <viklybi1@gmail.com>
2024-11-30 13:14:12 +0300

    HALYAVA2BALLA

---
+++ Lab4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string,int> students = new Dictionary<string, int>();
+        Random rnd = new Random();
+        string filePath = "astz.txt";
+        string[] lines = File.ReadAllLines(filePath);
+        string newFilePath = "students.txt";
+        int countForWhile = 0;
+        while (students.Count < lines.Length && countForWhile < lines.Length)
+        {
+            int number = rnd.Next(1, 50);
+            if (!students.ContainsValue(number))
+            {
+                students.Add(lines[countForWhile], number);
+                countForWhile++;
+            }
+        }
+        for (int i = 0; i < lines.Length; i++){
+            
+        }
+        using (StreamWriter writer = File.CreateText(newFilePath))
+        {
+            foreach (var stud in students)
+            {
+                writer.WriteLine($"{stud.Key}, {stud.Value}");
+            }
+        }
+        Console.WriteLine($"Файл '{newFilePath}' успешно создан.");
+    }
+}
